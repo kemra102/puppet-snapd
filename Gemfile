3154663a4a785693d8eca6ad73ab119edc41166f
Danny Roberts <danny@thefallenphoenix.net>
2018-09-11 20:56:41 +0100

    Fix tests

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ gem 'puppetlabs_spec_helper', '>= 1.0.0'
 gem 'puppet-lint', '>= 1.0.0'
 gem 'facter', '>= 1.7.0'
 gem 'rspec-puppet'
+gem 'rspec-puppet-facts', '~> 1.7', :require => false
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,20 +1,27 @@
 require 'spec_helper'
 
 describe 'snapd' do
-  context 'with default values for all parameters' do
+  on_supported_os.each do |os, fs_facts|
+    context "on #{os} with default values for all parameters" do
+      let :facts do
+        fs_facts
+      end
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('snapd') }
+      it { is_expected.to contain_package('snapd') }
+    end
+  end
+
+  context 'on ubuntu' do
     let(:facts) { {:os => { :family => 'Debian', :name => 'Ubuntu' } } }
 
-    it { is_expected.to compile }
-    it { is_expected.to contain_class('snapd') }
-    it { is_expected.to contain_package('snapd') }
     it { is_expected.to contain_service('snapd').with_ensure('running').with_enable(true).with_hasrestart(true).that_subscribes_to(['Package[snapd]']) }
   end
-  context 'on Arch linux' do
+
+  context 'on archlinux' do
     let(:facts) { {:os => { :family => 'Archlinux', :name => 'Archlinux' } } }
 
-    it { is_expected.to compile }
-    it { is_expected.to contain_class('snapd') }
-    it { is_expected.to contain_package('snapd') }
     it { is_expected.to contain_service('snapd.socket').with_ensure('running').with_enable(true).with_hasrestart(true).that_subscribes_to(['Package[snapd]']) }
   end
 end
